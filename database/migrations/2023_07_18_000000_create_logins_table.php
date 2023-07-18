99a20992f9bfe135fbae1607639759904a9754c2
allyans3 <a.sylenok@gmail.com>
2023-07-18 11:36:04 +0200

    Added support for UUID

--- database/migrations/2023_07_18_000000_create_logins_table.php
+++ database/migrations/2023_07_18_000000_create_logins_table.php
@@ -14,9 +14,14 @@ class CreateLoginsTable extends Migration
     public function up()
     {
         Schema::create(config('auth_tracker.table_name'), function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->morphs('authenticatable');
+            if (config('auth_tracker.id_type') === 'uuid') {
+                $table->uuid();
+                $table->uuidMorphs('authenticatable');
+            } else {
+                $table->id();
+                $table->morphs('authenticatable');
+            }
+
             $table->string('user_agent')->nullable();
             $table->string('ip')->nullable();
             $table->json('ip_data')->nullable();
@@ -31,6 +36,8 @@ class CreateLoginsTable extends Migration
             $table->string('remember_token')->nullable();
             $table->string('oauth_access_token_id')->nullable();
             $table->unsignedBigInteger('personal_access_token_id')->nullable();
+
+            $table->timestamps();
 
             $table->expirable('expires_at');
             $table->softDeletes();
