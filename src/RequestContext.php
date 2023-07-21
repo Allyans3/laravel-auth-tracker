ed3a5441bee3c90a41b58a832a007f6633114bca
allyans3 <a.sylenok@gmail.com>
2023-07-21 15:29:37 +0200

    Fix of fetching real client ip

--- src/RequestContext.php
+++ src/RequestContext.php
@@ -44,7 +44,26 @@ class RequestContext
         $this->ipProvider = IpProviderFactory::build(config('auth_tracker.ip_lookup.provider'));
 
         $this->userAgent = Request::userAgent();
-        $this->ip = Request::ip();
+        $this->ip = self::getIp();
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIp(): ?string
+    {
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    $ip = trim($ip); // just to be safe
+
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return request()->ip(); // it will return the server IP if the client IP is not found using this method.
     }
 
     /**
