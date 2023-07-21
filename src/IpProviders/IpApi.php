41db2fb5875621eb248f8d326976bf477ed3a650
allyans3 <a.sylenok@gmail.com>
2023-07-21 15:43:44 +0200

    Fix of fetching real client ip

--- src/IpProviders/IpApi.php
+++ src/IpProviders/IpApi.php
@@ -18,7 +18,26 @@ class IpApi implements IpProvider
      */
     public function getRequest()
     {
-        return new GuzzleRequest('GET', 'http://ip-api.com/json/' . Request::ip() . '?fields=25');
+        return new GuzzleRequest('GET', 'http://ip-api.com/json/' . self::getIp() . '?fields=25');
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getIp(): ?string
+    {
+        foreach (array('HTTP_CLIENT_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_X_CLUSTER_CLIENT_IP', 'HTTP_FORWARDED_FOR', 'HTTP_FORWARDED', 'REMOTE_ADDR') as $key){
+            if (array_key_exists($key, $_SERVER) === true) {
+                foreach (explode(',', $_SERVER[$key]) as $ip) {
+                    $ip = trim($ip); // just to be safe
+
+                    if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
+                        return $ip;
+                    }
+                }
+            }
+        }
+        return request()->ip(); // it will return the server IP if the client IP is not found using this method.
     }
 
     /**
